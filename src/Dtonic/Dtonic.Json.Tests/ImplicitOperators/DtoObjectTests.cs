8e5c6d66c2ef96145dfd8521f2e323e5763a8159
Colin Wilmans <colin.wilmans@floriade.com>
2024-02-07 14:20:06 +0100

    ImplicitOperators -> Added DtoObjectTests

---
+++ src/Dtonic/Dtonic.Json.Tests/ImplicitOperators/DtoObjectTests.cs
@@ -0,0 +1,52 @@
+﻿using Dtonic.Dto;
+using TestClasses;
+
+namespace ImplicitOperators;
+
+[TestClass]
+public class DtoObjectTests
+{
+    [TestMethod]
+    public void Assign_object()
+    {
+        //Arrange
+        var obj = new TestDto();
+
+        //Act
+        DtoObject<TestDto> dto = obj;
+
+        //Assert
+        Assert.IsTrue(dto.IsSpecified);
+        Assert.IsFalse(dto.IsNull);
+        Assert.IsFalse(dto.Value.aString.IsSpecified);
+    }
+
+    [TestMethod]
+    public void Assign_nullable_null()
+    {
+        //Arrange
+        TestDto? s = null;
+
+        //Act
+        DtoObject<TestDto> dto = s;
+
+        //Assert
+        Assert.IsTrue(dto.IsSpecified);
+        Assert.IsTrue(dto.IsNull);
+    }
+
+    [TestMethod]
+    public void Assign_nullable_object ()
+    {
+        //Arrange
+        TestDto? v = new TestDto();
+
+        //Act
+        DtoObject<TestDto> dto = v;
+
+        //Assert
+        Assert.IsTrue(dto.IsSpecified);
+        Assert.IsFalse(dto.IsNull);
+        Assert.IsFalse(dto.Value.aString.IsSpecified);
+    }
+}
